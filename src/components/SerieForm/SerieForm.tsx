39402f22e72f1e7f9369f392d30625769e0a6031
Giovanna <giovannabmarinho@outlook.com.br>
2025-08-24 17:20:32 -0300

    crud de series

--- src/components/SerieForm/SerieForm.tsx
+++ src/components/SerieForm/SerieForm.tsx
@@ -1,20 +1,84 @@
-import { useForm } from "react-hook-form"
-import { useParams } from "react-router"
+import { useEffect } from "react";
+import { useForm, type Resolver } from "react-hook-form"
+import { useNavigate, useParams } from "react-router"
 
 
-interface CamposSerie {
+export interface CamposSerie {
   nome: string
+  numTemporadas: number;
+  dataLancamentoTemporada: string
+  diretor: string
+  produtora: string
+  categoria: string
+  dataAssistiu: string
 }
+
+const resolver: Resolver<CamposSerie> = async (values) => {
+
+  const errors: {[key in keyof CamposSerie]?: any} = {}
+
+  if (!values.nome) {
+    errors["nome"] = {
+      type: "required",
+      message: "This is required.",
+    }
+  }
+
+  return {
+    values,
+    errors
+  }
+}
+
 
 export function SerieForm() {
   const params = useParams()
-  const {register, handleSubmit, formState: {errors} } = useForm<CamposSerie>()
+  const navigate = useNavigate()
+  const {register, handleSubmit, formState: {errors}, setValue } = useForm<CamposSerie>({resolver})
 
   const onSubmit = handleSubmit(data => {
     console.log("dados do form", data)
+
+    const seriesCadastradas = JSON.parse(window.localStorage.getItem("series") ?? "[]") as CamposSerie[]
+    const id = params["id"]
+    if (id) {
+      seriesCadastradas[Number(id)] = data
+    } else {
+      seriesCadastradas.push(data)
+    }
+    
+    window.localStorage.setItem("series", JSON.stringify(seriesCadastradas))
+
+    navigate("/lista")
   })
 
-  return <div>
+  useEffect(() => {
+    const id = params["id"]
+    if (id) {
+      const seriesCadastradas = JSON.parse(window.localStorage.getItem("series") ?? "[]") as CamposSerie[]
+      const serie = seriesCadastradas[Number(id)]
+
+      if (serie) {
+        setValue("nome", serie.nome)
+        setValue("categoria", serie.categoria)
+        setValue("dataAssistiu", serie.dataAssistiu)
+        setValue("dataLancamentoTemporada", serie.dataLancamentoTemporada)
+        setValue("diretor", serie.diretor)
+        setValue("numTemporadas", serie.numTemporadas)
+        setValue("produtora", serie.produtora)
+      }
+    }
+  }, [params])
+
+  return <form style={{display: "flex", flexDirection: "column"}} onSubmit={onSubmit}>
     <input {...register("nome")} placeholder="Nome da série" />
-  </div>
+    <input {...register("numTemporadas")} type="number" placeholder="Número de temporadas" />
+    <input {...register("dataLancamentoTemporada")} type="date" placeholder="Data de Lançamento da Temporada" />
+    <input {...register("diretor")} placeholder="Diretor" />
+    <input {...register("produtora")} placeholder="Produtora" />
+    <input {...register("categoria")} placeholder="Categoria" />
+    <input {...register("dataAssistiu")} type="date" placeholder="Data em que assistiu" />
+
+    <button type="submit">Cadastrar Série</button>
+  </form>
 }
--- src/components/SerieList/SerieList.tsx
+++ src/components/SerieList/SerieList.tsx
@@ -1,3 +1,40 @@
+import { useEffect, useState } from "react"
+import type { CamposSerie } from "../SerieForm/SerieForm"
+import { useNavigate } from "react-router"
+
 export function SerieList () {
-    return <p>Olá</p>
+    const navigate = useNavigate()
+    const [series, setSeries] = useState<CamposSerie[]>([])
+
+    useEffect(() => {
+        const seriesCadastradas = JSON.parse(window.localStorage.getItem("series") ?? "[]") as CamposSerie[]
+        setSeries(seriesCadastradas)
+    }, [])
+
+    const handleEditarSerie = (index: number) => {
+        navigate("/serie/" + index)
+    }
+
+    const handleDeletarSerie = (index: number) => {
+        const seriesAtualizadas = [...series]
+        seriesAtualizadas.splice(index)
+        setSeries(seriesAtualizadas)
+        window.localStorage.setItem("series", JSON.stringify(seriesAtualizadas))
+    }
+
+    return <div>
+        {series.map((serie, index) => {
+            return <div key={index}>
+                <p>{serie.nome}</p>
+                <p>{serie.numTemporadas} temporadas</p>
+                <p>{new Date(serie.dataLancamentoTemporada).toDateString()}</p>
+                <p>{serie.diretor}</p>
+                <p>{serie.produtora}</p>
+                <p>{serie.categoria}</p>
+                <p>{new Date(serie.dataAssistiu).toDateString()}</p>
+                <button type="button" onClick={() => handleEditarSerie(index)}>Editar</button>
+                <button type="button" onClick={() => handleDeletarSerie(index)}>Excluir</button>
+            </div>
+        })}
+    </div>
 }
